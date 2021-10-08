307fb87e6686968a676952563cb721317c087c6a
Gianmarco Venturini <GIANMARCO.VENTURINI@local.ittcesena.it>
2021-10-08 08:36:31 +0200

    aggiunta commenti

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,22 +6,21 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+            //creazione dell'array di interi
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
-
+            //chiamata metodo ordinamento
             ordina(vettore);
-
+            //stampa array
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(vettore[i]);
 
             Console.ReadKey();
         }
-
+        //metodo per l'ordinamento
         static void ordina (int[] vettore)
         {
             Array.Sort(vettore);
-        }
-
-     
+        }     
     }
 }
